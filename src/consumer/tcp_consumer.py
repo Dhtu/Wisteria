e679ec44350bf5de5c649a85336b873eca33130c
thtu <thtu@foxmail.com>
2021-02-24 23:37:26 +0800

    tcp agent 1.1

---
+++ src/consumer/tcp_consumer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from kafka import KafkaConsumer
+import sys
+
+def print_byte(s, file=sys.stdout, nl=1):
+    """
+    printb(s)
+
+    print a bytes object to stdout and flush
+    """
+    buf = file.buffer if hasattr(file, "buffer") else file
+
+    buf.write(s)
+    if nl:
+        buf.write(b"\n")
+    file.flush()
+
+consumer = KafkaConsumer('quickstart-events', bootstrap_servers= ['localhost:9092'])
+for msg in consumer:
+    print_byte(msg.value)
