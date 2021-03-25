db02ceb650381a9a6780e43bae9c6eb217b4d447
thtu <thtu@foxmail.com>
2021-03-25 16:39:43 +0800

    before refining tps

--- PingPong/client.py
+++ PingPong/client.py
@@ -13,7 +13,7 @@ from pygen.ping import PingService
 try:
 
     # Make socket
-    transport = TSocket.TSocket('localhost', 9090)
+    transport = TSocket.TSocket('172.17.0.1', 9090)
 
     # Buffering is critical. Raw sockets are very slow
     transport = TTransport.TBufferedTransport(transport)
