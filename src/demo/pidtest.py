83b506f8b7f875e4b918bf8f7f58e2dc103fdff9
thtu <thtu@foxmail.com>
2021-04-15 21:03:47 +0800

    fork test

--- src/demo/pidtest.py
+++ src/demo/pidtest.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python
-
-import os
-
-print(os.getpid())
+from bcc import BPF
+BPF(text='TRACEPOINT_PROBE(syscalls, sys_exit_fork) { bpf_trace_printk("Hello, World!\\n"); return 0; }').trace_print()
